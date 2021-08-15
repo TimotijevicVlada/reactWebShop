2f34d552dc0564dcfede7efc64dc7c6386990756
Vladimir <timotijevicvlada@gmail.com>
2021-08-15 20:33:08 +0200

    Contact page updated. Cover photo added.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => {
         const showSignup = document.getElementsByClassName("signup_container")[0];
         const hideLogin = document.getElementsByClassName("login_container")[0];
         setShow(
-            hideLogin.style.display = "block",
+            hideLogin.style.display = "flex",
             showSignup.style.display = "none"
             );
     }
@@ -24,12 +24,28 @@ const Contact = () => {
     return (
         <div className="contact-container">
             <div className="login_container">
-                <h2>Login</h2>
-                <div><input type="text" placeholder="Name"/></div>
-                <div><input type="email" placeholder="Email"/></div>
-                <div><input type="password" placeholder="Password"/></div>
-                <button>Login</button>
-                <p onClick={displaySignup} className="display_signup">Create account</p>
+                <div className="login">
+                    <h2>Login</h2>
+                    <div><input type="text" placeholder="Name"/></div>
+                    <div><input type="email" placeholder="Email"/></div>
+                    <div><input type="password" placeholder="Password"/></div>
+                    <button>Login</button>
+                    <p onClick={displaySignup} className="display_signup">Create account</p>
+                </div>
+                <div className="send_message">
+                    <h2>Send us message</h2>
+                    <div><input type="text" placeholder="Subject"/></div>
+                    <div><textarea rows="7" cols="50" placeholder="Type message"/></div>
+                    <button>Send message</button>
+                </div>
+                <div className="contact_us">
+                    <h2>Contact us</h2>
+                    <div><i class="fas fa-map-marker-alt"></i>Knez Mihailova 40b</div>
+                    <div><i class="fas fa-globe-europe"></i>Belgrade, Serbia</div>
+                    <div><i class="fas fa-building"></i>Workdays 7 days 24 hours</div>
+                    <div><i class="fas fa-phone-alt"></i>+381631188180</div>
+                    <div><i class="far fa-envelope"></i>timotijevicvlada@gmail.com</div>
+                </div>
             </div>
             <div className="signup_container">
                 <h2>Signup</h2>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    const products = useSelector(state => state.ProductsReducer.products);
-    //console.log(products)
+  const products = useSelector((state) => state.ProductsReducer.products);
+  //console.log(products)
 
-    return (
-        <div className="home_container">
-            {products.map(elem => (
-                <div className="product-container" key={elem.id} >
-                    <div className="product">
-                        <div className="product-img">
-                            <Link to={`/details/${elem.id}`}>
-                                <img src={`images/${elem.image}`} alt="image name"/>
-                            </Link>
-                        </div>
-                        <div className="product-name">
-                            <h2>{elem.name}</h2>
-                        </div>
-                        <div className="price-container">
-                            <div className="price">${elem.price}</div>
-                            <div>${elem.discountPrice}</div>
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="cover_pic"></div>
+      <div className="product_title">Shirts</div>
+      <div className="home_container">
+        {products.map((elem) => (
+          <div className="product-container" key={elem.id}>
+            <div className="product">
+              <div className="product-img">
+                <Link to={`/details/${elem.id}`}>
+                  <img src={`images/${elem.image}`} alt="image name" />
+                </Link>
+              </div>
+              <div className="product-name">
+                <h2>{elem.name}</h2>
+              </div>
+              <div className="price-container">
+                <div className="price">${elem.price}</div>
+                <div>${elem.discountPrice}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
